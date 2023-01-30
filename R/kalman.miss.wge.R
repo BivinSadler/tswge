c76fb8aa02e6f3b25607c7ae67063c376da93247
loaner <bsnatr@hotmail.com>
2023-01-30 16:34:42 -0600

    Fix to kalman and fore.arima.wge xs to XS, etc.

--- R/kalman.miss.wge.R
+++ R/kalman.miss.wge.R
@@ -4,12 +4,12 @@ kalman.miss.wge=function(y,start,gam0,F,gamV,Gtmiss,gamW){
   sqgamW=sqrt(gamW)
   chgamW=chol(gamW)
   yy=y
-  
   #Fix from David Stoffer 1-29-2023
   result=astsa::Ksmooth(y=yy, A=Gtmiss, mu0=start, Sigma0=gam0, Phi=F, sQ=sqgamV, sR=chgamW)
   # end fix
   
-  pfs=cbind(yy,result$xp,result$Pp,result$xf,result$Pf,result$xs,result$Ps)
+  #Bivin fixed xp, xf and xs to Xp, Xf and Xs below 1-20-2023
+  pfs=cbind(yy,result$Xp,result$Pp,result$Xf,result$Pf,result$Xs,result$Ps)
   colnames(pfs)=c("Data","Prediction","Var_Predict","Filter","Var_Filter","Smooth","Var_Smooth")
   return(pfs)
 }
--- R/kalman.wge.R
+++ R/kalman.wge.R
@@ -5,7 +5,7 @@ kalman.wge=function(y,start,gam0,F,gamV,G,gamW){
   yy=y
   #Fix from David Stoffer 1-29-2023
   result=astsa::Ksmooth(y=yy, A=G, mu0=start, Sigma0=gam0, Phi=F, sQ=sqgamV, sR=sqgamW)
-  pfs=cbind(yy,result$xp,result$Pp,result$xf,result$Pf,result$xs,result$Ps)
+  pfs=cbind(yy,result$Xp,result$Pp,result$Xf,result$Pf,result$Xs,result$Ps)
   colnames(pfs)=c("Data","Prediction","Var_Predict","Filter","Var_Filter","Smooth","Var_Smooth")
   return(pfs)
 }
